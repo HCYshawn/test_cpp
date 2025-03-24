b4889f286e3229036553255ab443c99de72c6941
南天的波江 <1754099901@qq.com>
2025-03-24 21:07:30 +0800

    测试内置类型是否具有构造函数

---
+++ 2025/3 month/3_24_1/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i();
+    int j(1);
+    int k(2);
+
+    double x();
+    double y(1.1);
+    double z(2.2);
+
+    cout << i << " " << j << " " << k << endl;
+    cout << x << " " << y << " " << z << endl;
+    return 0;
+}
