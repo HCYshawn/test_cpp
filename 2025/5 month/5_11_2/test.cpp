1af5c7b7edbef82ed725fabf9f16319763906fcb
南天的波江 <1754099901@qq.com>
2025-05-11 20:09:29 +0800

    RAII测试指针

---
+++ 2025/5 month/5_11_2/test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdexcept>
+#include <memory>
+using namespace std;
+
+template <class T>
+class SmartPtr
+{
+public:
+    SmartPtr(T *p = nullptr) : _ptr(p)
+    {
+    }
+    ~SmartPtr()
+    {
+        if (_ptr)
+            delete _ptr;
+    }
+
+private:
+    T *_ptr;
+};
+
+int div()
+{
+    int a, b;
+    cin >> a >> b;
+    if (b == 0)
+    {
+        throw invalid_argument("³ý0´íÎó");
+    }
+    return a / b;
+}
+
+void Func()
+{
+    SmartPtr<int> sp1(new int);
+    SmartPtr<int> sp2(new int);
+
+    cout << div() << endl;
+}
+
+int main()
+{
+    try
+    {
+        Func();
+    }
+    catch (const exception &e)
+    {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
